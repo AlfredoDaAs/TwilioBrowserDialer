3f438d1ff849edc165f9997e948d9f909a917299
Alfredo Dávila Assad <alfredo.davila@codigomx.io>
2020-11-01 17:07:10 -0600

    setting express app

--- firebase-functions/functions/src/index.ts
+++ firebase-functions/functions/src/index.ts
@@ -1,9 +1,12 @@
 import * as functions from 'firebase-functions';
+import * as express from 'express';
+import * as bodyParser from "body-parser";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+//initialize express server
+const app = express();
+
+//add the path to receive request and set json as bodyParser to process the body 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+export const api = functions.https.onRequest(app);
