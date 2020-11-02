e3648426fc3570245bd22cc6b568ad84b95e79d0
Alfredo Dávila Assad <alfredo.davila@codigomx.io>
2020-11-01 21:06:48 -0600

    front configs

---
+++ static-site/app/vue.config.js
@@ -0,0 +1,30 @@
+// vue.config.js
+module.exports = {
+    // options...
+    chainWebpack: (config) => {
+
+        config.module
+            .rule('vue')
+            .use('vue-loader')
+            .loader('vue-loader')
+            .tap(options => {
+                options.transformAssetUrls = {
+                    video: ['src', 'poster'],
+                    source: 'src',
+                    img: 'src',
+                    image: ['xlink:href', 'href'],
+                    use: ['xlink:href', 'href'],
+                    'b-avatar': 'src',
+                    'b-img': 'src',
+                    'b-img-lazy': ['src', 'blank-src'],
+                    'b-card': 'img-src',
+                    'b-card-img': 'src',
+                    'b-card-img-lazy': ['src', 'blank-src'],
+                    'b-carousel-slide': 'img-src',
+                    'b-embed': 'src'
+                }
+
+                return options
+            })
+    }
+}
