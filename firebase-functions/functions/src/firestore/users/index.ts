3d85a69e29884648f8f6c8f26504ade842ce016e
Alfredo Dávila Assad <alfredo.davila@codigomx.io>
2020-11-02 08:57:54 -0600

    user collection class

---
+++ firebase-functions/functions/src/firestore/users/index.ts
@@ -0,0 +1,16 @@
+import AbstractCollection from '../../firebase/AbstractCollection'
+import { throwUntrustedClientError } from '../../common/FriendlyError'
+
+const collectionName = 'users'
+
+class Collection extends AbstractCollection {
+    public defaultValues: any = {
+        name: null,
+        email: null,
+        phoneNumber: '',
+        isAdmin: false,
+        metadata: null
+    }
+}
+
+export default new Collection(collectionName)
