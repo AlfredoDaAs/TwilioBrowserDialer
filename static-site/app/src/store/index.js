b97020007d336c55b65bd09d2ab8b30b25f1ab0a
Alfredo Dávila Assad <alfredo.davila@codigomx.io>
2020-11-07 22:31:31 -0600

    config cookies and persisted state on vuex

--- static-site/app/src/store/index.js
+++ static-site/app/src/store/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from "vuex-persistedstate";
+import * as Cookies from "js-cookie";
 
 import token from './token'
 import auth from './auth'
@@ -7,8 +9,17 @@ import auth from './auth'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  modules: {
-    token,
-    auth
-  }
+    plugins: [createPersistedState({
+        storage: {
+            getItem: (key) => Cookies.get(key),
+            // Please see https://github.com/js-cookie/js-cookie#json, on how to handle JSON.
+            setItem: (key, value) =>
+              Cookies.set(key, value, { expires: 3, /* secure: true */ }),
+            removeItem: (key) => Cookies.remove(key),
+          },
+    })],
+    modules: {
+        token,
+        auth
+    }
 })
