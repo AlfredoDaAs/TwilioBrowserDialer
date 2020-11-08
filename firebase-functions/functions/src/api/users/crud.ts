ee9ae1cc34bd96b012a07d7eca193fe8bcc052ff
Alfredo Dávila Assad <alfredo.davila@codigomx.io>
2020-11-08 14:58:56 -0600

    get users request

--- firebase-functions/functions/src/api/users/crud.ts
+++ firebase-functions/functions/src/api/users/crud.ts
@@ -1,15 +1,16 @@
 import * as express from 'express'
-import { adminMiddleware } from '../../middlewares/admin'
+//import { adminMiddleware } from '../../middlewares/admin'
+import User from '../../firestore/users'
 
 const router = express.Router()
 
-//needs admin middleware
+router.get('/', async (req, res) => {
+    const users = await User.getAllUsers()
 
-router.get('/', (req, res) => {
-
+    res.json(users)
 })
 
-router.post('/', adminMiddleware, (req, res) => {
+/* router.post('/', adminMiddleware, (req, res) => {
 
 })
 
@@ -19,6 +20,6 @@ router.put('/:id', adminMiddleware, (req, res) => {
 
 router.delete('/:id', adminMiddleware, (req, res) => {
 
-})
+}) */
 
 export default router
