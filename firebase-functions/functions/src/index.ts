2a8eb77714e931ba803a0b242e9a5fbbd61b34f1
Alfredo Dávila Assad <alfredo.davila@codigomx.io>
2020-11-08 14:57:52 -0600

    cofigured cors

--- firebase-functions/functions/src/index.ts
+++ firebase-functions/functions/src/index.ts
@@ -1,6 +1,7 @@
 import * as functions from 'firebase-functions';
 import * as express from 'express';
 import * as bodyParser from "body-parser";
+import * as cors from 'cors'
 
 import { initializeFirebase } from './firebase'
 
@@ -17,16 +18,7 @@ initializeFirebase();
 //initialize express server
 const app = express();
 
-//For CORS
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,DELETE,PUT,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Authorization, Accept');
-    //req.decoded = { admn: true }
-
-    next();
-});
-
+app.use(cors({ origin: true }))
 //add the path to receive request and set json as bodyParser to process the body 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
