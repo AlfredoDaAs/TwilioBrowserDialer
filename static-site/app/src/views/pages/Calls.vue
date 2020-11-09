01b859fd2a8624365608bafa7ae24989cbb2a8ed
Alfredo Dávila Assad <alfredo.davila@codigomx.io>
2020-11-08 22:34:47 -0600

    view redesign

--- static-site/app/src/views/pages/Calls.vue
+++ static-site/app/src/views/pages/Calls.vue
@@ -1,19 +1,29 @@
 <script>
 import { Device } from 'twilio-client'
+import axios from "../../axios";
 
 export default {
     name: 'Calls',
     data: () => ({
         countryCode: '52',
-        currentNumber: '3323436466',
+        currentNumber: '',
         onPhone: false,
         muted: false,
         connection: null,
         log: 'Connecting...',
     }),
     methods: {
-        toggleCall() {
+        async getToken() {
+            const result = await axios.get("/token");
+
+            if (result.data) {
+                const token = result.data.token;
+                this.$store.dispatch("setToken", token);
+            }
+        },
+        async toggleCall() {
             if (!this.onPhone) {
+                await this.getToken()
                 this.muted = false;
                 this.onPhone = true;
                 // make outbound call with current number
@@ -37,24 +47,33 @@ export default {
 <template>
     <b-container>
         <b-row>
-            <b-col>
-                <h3>Make a Call</h3>
+            <b-col md="6">
+                <b-card title="Make a Call">
+                    <div class="d-flex">
+                        <div style="width: 75%">
+                            <b-form-input max="10" v-model="currentNumber" type="number" placeholder="Enter your phone"></b-form-input>
+                        </div>
+                        <div>
+                            <b-button class="mx-2" variant="success" @click="toggleCall" v-if="!onPhone">
+                                <b-icon-telephone></b-icon-telephone>
+                            </b-button>
+                            <b-button class="mx-2" variant="danger" @click="toggleCall" v-else>
+                                <b-icon-telephone-x></b-icon-telephone-x>
+                            </b-button>
+                            <b-button @click="toggleMute">
+                                <b-icon-mic v-if="!muted"></b-icon-mic>
+                                <b-icon-mic-mute v-else></b-icon-mic-mute>
+                            </b-button>
+                        </div>
+                    </div>
+                    <b-alert class="mt-5" show variant="info">{{ onPhone ? log : 'Call status' }}</b-alert>
+                </b-card>
+            </b-col>
+            <b-col md="6">
+                <b-card title="Call queue">
+
+                </b-card>
             </b-col>
         </b-row>
-        <b-row class="mt-4">
-            <b-col md="8">
-                <b-form-input max="10" v-model="currentNumber" type="tel" placeholder="Enter your phone"></b-form-input>
-            </b-col>
-            <b-col md="2">
-                <b-button @click="toggleCall">{{ onPhone ? 'Close Call' : 'Call' }}</b-button>
-            </b-col>
-            <b-col>
-                <b-button @click="toggleMute">
-                    <b-icon-mic v-if="!muted"></b-icon-mic>
-                    <b-icon-mic-mute v-else></b-icon-mic-mute>
-                </b-button>
-            </b-col>
-        </b-row>
-        <b-alert class="mt-5" show variant="info">{{ onPhone ? log : 'Make a Call!' }}</b-alert>
     </b-container>
 </template>
