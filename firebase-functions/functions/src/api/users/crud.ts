178947ae75bc9f4d03fcd4d646692ce76323eded
Alfredo Dávila Assad <alfredo.davila@codigomx.io>
2020-11-08 21:38:35 -0600

    users get and post working

--- firebase-functions/functions/src/api/users/crud.ts
+++ firebase-functions/functions/src/api/users/crud.ts
@@ -1,20 +1,36 @@
 import * as express from 'express'
-//import { adminMiddleware } from '../../middlewares/admin'
+import { adminMiddleware } from '../../middlewares/admin'
 import User from '../../firestore/users'
 
 const router = express.Router()
 
 router.get('/', async (req, res) => {
-    const users = await User.getAllUsers()
+    try {
+        const users = await User.getAllUsers()
 
-    res.json(users)
+        res.json(users)
+    } catch (error) {
+        throw new Error(error);
+    }
 })
 
-/* router.post('/', adminMiddleware, (req, res) => {
+router.post('/', adminMiddleware, async (req, res) => {
+    try {
+        const body = req.body
 
+        const result = await User.createOne({
+            name: body.name,
+            email: body.email,
+            'twilio.number': body.twilioNumber
+        })
+
+        res.json(result)
+    } catch (error) {
+        throw new Error(error);
+    }
 })
 
-router.put('/:id', adminMiddleware, (req, res) => {
+/* router.put('/:id', adminMiddleware, (req, res) => {
 
 })
 
