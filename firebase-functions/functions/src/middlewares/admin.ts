11efd5bd86076ae774269e814ce8e995cd7a3e75
Alfredo Dávila Assad <alfredo.davila@codigomx.io>
2020-11-08 21:44:19 -0600

    forgot to read decoded

--- firebase-functions/functions/src/middlewares/admin.ts
+++ firebase-functions/functions/src/middlewares/admin.ts
@@ -1,7 +1,7 @@
 import * as express from 'express'
 
 export const adminMiddleware = (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    if(req.isAdmin) return next()
+    if(req.decoded.isAdmin) return next()
 
     res.status(403)
         .json({
