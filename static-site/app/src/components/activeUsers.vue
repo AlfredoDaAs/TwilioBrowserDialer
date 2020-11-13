cc4027c8f257d6b70b2d2736ed328a17718846d3
Alfredo Dávila Assad <alfredo.davila@codigomx.io>
2020-11-13 11:54:50 -0600

    rt connected users log

--- static-site/app/src/components/activeUsers.vue
+++ static-site/app/src/components/activeUsers.vue
@@ -1,6 +1,36 @@
 <script>
+import firebase from "firebase/app";
+import { mapGetters } from 'vuex';
+
 export default {
-    name: 'ActiveUsers'
+    name: 'ActiveUsers',
+    data: () => ({
+        users: []
+    }),
+    computed: mapGetters(['getUserId']),
+    methods: {
+        listenUsers() {
+            firebase.database().ref('users').on('value', (items) => {
+                let usersList = []
+                items.forEach(item => {
+                    const key = item.key
+                    const data = item.val()
+                    if(key === this.getUserId && data.status === 'online') {
+                        usersList.push({
+                            key: key,
+                            name: data.name,
+                            status: data.status
+                        })
+                    }
+                })
+
+                this.users = usersList;
+            })
+        }
+    },
+    created() {
+        this.listenUsers()
+    }
 }
 </script>
 
@@ -9,8 +39,11 @@ export default {
         <b-card id="usersCardList" header="Online Users">
             <div id="activeUsersBox">
                 <b-list-group>
-                    <b-list-group-item class="d-flex align-items-center" v-for="n in 20" :key="n">
-                        User #{{n}}
+                    <b-list-group-item class="d-flex justify-content-between" v-for="user in users" :key="user.key">
+                        <span>{{ user.name }}</span>
+                        <div>
+                            <b-badge variant="success">Connected</b-badge>
+                        </div>
                     </b-list-group-item>
                 </b-list-group>
             </div>
