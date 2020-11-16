d37f95ce2313592cde4f8bced88997969a424332
Alfredo Dávila Assad <alfredo.davila@codigomx.io>
2020-11-15 21:54:42 -0600

    improved functionality with less api calls

--- static-site/app/src/views/pages/Calls.vue
+++ static-site/app/src/views/pages/Calls.vue
@@ -3,22 +3,60 @@ import { Device } from 'twilio-client';
 import axios from "../../axios";
 import firebase from "firebase/app";
 import { mapGetters } from 'vuex';
+import { handleError } from '../../handleErrors'
 
 export default {
     name: 'Calls',
     data: () => ({
-        countryCode: '52',
-        currentNumber: '',
+        selectUser: null,
         onPhone: false,
         muted: false,
         connection: null,
         log: 'Connecting...',
         queueCalls: [],
         selectTransfer: null,
-        users: []
+        transferUsers: [],
+        usersToCall: [],
+        usersData: []
     }),
     computed: mapGetters(['getUserId']),
     methods: {
+        async getUsers() {
+            try {
+                const result = await axios.get('/users')
+
+                if(result.data) {
+                    const users = result.data
+
+                    users.forEach(user => {
+                        this.usersData[user.id] = user
+                    })
+                    const userOptions = users.map(user => ({
+                        value: user.id,
+                        text: `${user.name} ${user.lastName}`
+                    }))
+
+                    userOptions.unshift({ value: null, text: 'Call to...' })
+                    this.usersToCall = userOptions
+                }
+            } catch (error) {
+                handleError(error, this, 'danger');
+            }
+        },
+        async getUser(id) {
+            try {
+                if(id)  {
+                    const result = await axios.get(`users/${id}`)
+
+                    return result.data
+                }
+                else {
+                    return null
+                }
+            } catch (error) {
+                handleError(error, this, 'danger');
+            }
+        },
         async getToken() {
             const result = await axios.get("/token");
 
@@ -27,15 +65,17 @@ export default {
                 this.$store.dispatch("setToken", token);
             }
         },
-        async toggleCall() {
-            if (!this.onPhone) {
-                await this.getToken()
-                this.muted = false;
-                this.onPhone = true;
-                // make outbound call with current number
-                var n = '+' + this.countryCode + this.currentNumber.replace(/\D/g, '');
-                this.connection = Device.connect({ number: n });
-                this.log = 'Calling ' + n;
+        toggleCall() {
+            if (!this.onPhone && this.selectUser) {
+                const user = this.usersData[this.selectUser]
+                if(user) {
+                    this.muted = false;
+                    this.onPhone = true;
+                    // make outbound call with current number
+                    var n = '+' + user.phoneNumber.replace(/\D/g, '');
+                    this.connection = Device.connect({ number: n });
+                    this.log = 'Calling ' + n;
+                }
             } else {
                 // hang up call in progress
                 Device.disconnectAll();
@@ -53,8 +93,18 @@ export default {
                 }
 
                 const data = user.val()
-                console.log('data', data);
-                this.queueCalls = data.calls
+                let callsObjs = []
+                for (const key in data.calls) {
+                    const call = data.calls[key]
+                    const user = this.usersData[key]
+                    callsObjs.push({
+                        id: user.id,
+                        date: call.date,
+                        name: `${user.name} ${user.lastName}`
+                    })
+                }
+                const sortedCalls = callsObjs.sort((a, b) => a.date - b.date)
+                this.queueCalls = sortedCalls
             })
         },
         listenUsers() {
@@ -72,29 +122,37 @@ export default {
                 })
 
                 usersList.unshift({ value: null, text: 'User to Transfer' })
-                this.users = usersList;
+                this.transferUsers = usersList;
             })
         },
         transferTo() {
-            if(this.selectTransfer && this.currentNumber) {
+            if(this.selectTransfer && this.selectUser && this.selectUser !== this.getUserId) {
+                const user = this.usersData[this.selectUser]
                 const userRef = firebase.database().ref(`users/${this.selectTransfer}`)
                 userRef.child('calls').update({
-                    [this.currentNumber]: true
+                    [this.selectUser]: {
+                        date: firebase.database.ServerValue.TIMESTAMP,
+                        name: `${user.name} ${user.lastName}`
+                    }
                 })
                 this.selectTransfer = null
             }
         },
-        deleteFromQueue(number) {
+        deleteFromQueue(id) {
             const userRef = firebase.database().ref(`users/${this.getUserId}`)
-            userRef.child('calls').child(number).remove()
+            userRef.child('calls').child(id).remove()
         },
-        takeCall(number) {
-            this.currentNumber = number
+        takeCall(id) {
+            this.selectUser = id
         }
     },
     created() {
-        this.getQueueCalls();
+        this.getToken();
         this.listenUsers();
+    },
+    async mounted() {
+        await this.getUsers();
+        this.getQueueCalls();
     }
 }
 </script>
@@ -106,7 +164,11 @@ export default {
                 <b-card title="Make a Call">
                     <div class="d-flex">
                         <div style="width: 75%">
-                            <b-form-input max="10" v-model="currentNumber" type="number" placeholder="Enter your phone"></b-form-input>
+                            <b-form-select
+                                v-model="selectUser"
+                                :options="usersToCall"
+                            >
+                            </b-form-select>
                         </div>
                         <div>
                             <b-button class="mx-2" variant="success" @click="toggleCall" v-if="!onPhone">
@@ -125,8 +187,7 @@ export default {
                         <div style="width: 75%">
                             <b-form-select
                                 v-model="selectTransfer"
-                                :options="users"
-                                placeholder=""
+                                :options="transferUsers"
                             >
                             </b-form-select>
                         </div>
@@ -140,12 +201,12 @@ export default {
             <b-col md="6">
                 <b-card title="Call queue">
                     <b-list-group>
-                        <b-list-group-item v-for="(call, number, i) in queueCalls" :key="`${i}-${number}`">
+                        <b-list-group-item v-for="(call, i) in queueCalls" :key="`${i}-${call.number}`">
                             <div class="d-flex justify-content-between">
-                                <span>{{number}}</span>
+                                <span>{{call.name}}</span>
                                 <div>
-                                    <b-button variant="success" size="sm" @click="takeCall(number)">take call</b-button>
-                                    <b-button variant="danger" class="ml-2" size="sm" @click="deleteFromQueue(number)">Remove</b-button>
+                                    <b-button variant="success" size="sm" @click="takeCall(call.id)">take call</b-button>
+                                    <b-button variant="danger" class="ml-2" size="sm" @click="deleteFromQueue(call.id)">Remove</b-button>
                                 </div>
                             </div>
                         </b-list-group-item>
