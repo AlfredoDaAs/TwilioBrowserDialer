30e13d90e10f5998a8ef81c9749dc7f569c543dc
Alfredo Dávila Assad <alfredo.davila@codigomx.io>
2020-11-15 21:52:58 -0600

    user creation update

--- static-site/app/src/components/UserCreateForm.vue
+++ static-site/app/src/components/UserCreateForm.vue
@@ -1,15 +1,21 @@
 <script>
 import axios from '../axios'
 import { required, email, numeric, minValue } from 'vuelidate/lib/validators'
+import { handleError } from '../handleErrors'
 
 export default {
     name: 'UserCreateForm',
     data: () => ({
         name: '',
+        lastname: '',
         email: '',
-        twilioNumber: ''
+        phoneNumber: '',
+        deparment:  '',
     }),
     validations: {
+        lastname: {
+            required
+        },
         name: {
             required
         },
@@ -17,10 +23,8 @@ export default {
             required,
             email
         },
-        twilioNumber: {
-            required,
-            numeric,
-            minValue: minValue(10)
+        deparment: {
+            required
         }
     },
     methods: {
@@ -28,13 +32,15 @@ export default {
             try {
                 this.$v.$touch()
                 if(this.$v.$invalid) {
-                    console.log('launch a toast error message');
+                    handleError(new Error('Missing required fields'), this, 'danger')
                 }
                 else {
                     const result = await axios.post('/users', {
                         name: this.name,
+                        lastname: this.lastname,
                         email: this.email,
-                        twilioNumber: this.twilioNumber
+                        phoneNumber: this.phoneNumber,
+                        deparment: this.deparment
                     })
 
                     if(result.data) {
@@ -43,14 +49,16 @@ export default {
                     }
                 }
             } catch (error) {
-                console.log('error', error.message);
+                handleError(error, this, 'danger')
             }
         },
         onReset() {
             // Reset our form values
             this.email = ''
-            this.twilioNumber = ''
+            this.phoneNumber = ''
             this.name = ''
+            this.lastname = ''
+            this.deparment = ''
 
             this.$nextTick(() => {
                 this.$v.$reset();
@@ -63,34 +71,49 @@ export default {
 <template>
     <b-form @submit.stop.prevent="onSubmit" @reset="onReset">
         <b-row>
-            <b-col>
+            <b-col md="6" sm="12">
                 <b-form-input
                     v-model="name"
                     type="text"
-                    required
                     placeholder="Enter name"
                     :state="$v.name.$dirty ? !$v.name.$error : null"
                 ></b-form-input>
             </b-col>
-            <b-col>
+            <b-col md="6" sm="12">
                 <b-form-input
-                    v-model="email"
-                    type="email"
-                    required
-                    placeholder="Enter email"
-                    :state="$v.email.$dirty ? !$v.email.$error : null"
+                    v-model="lastname"
+                    type="text"
+                    placeholder="Enter lastname"
+                    :state="$v.lastname.$dirty ? !$v.lastname.$error : null"
                 ></b-form-input>
             </b-col>
         </b-row>
         <b-row class="pt-3">
-            <b-col offset-md="6" md="6" sm="12">
+            <b-col md="6" sm="12">
+                <b-form-input
+                    v-model="email"
+                    type="email"
+                    placeholder="Enter email"
+                    :state="$v.email.$dirty ? !$v.email.$error : null"
+                ></b-form-input>
+            </b-col>
+            <b-col md="6" sm="12">
                 <b-form-input
                     min="10"
-                    v-model="twilioNumber"
+                    v-model="phoneNumber"
                     type="number"
-                    required
-                    placeholder="Twilio number"
-                    :state="$v.twilioNumber.$dirty ? !$v.twilioNumber.$error : null"
+                    placeholder="Enter number"
+                ></b-form-input>
+            </b-col>
+        </b-row>
+        <b-row class="pt-3">
+            <b-col md="6" sm="12">
+                <b-form-input
+                    min="10"
+                    v-model="deparment"
+                    type="text"
+                    placeholder="Enter deparment"
+                    :state="$v.deparment.$dirty ? !$v.deparment.$error : null"
                 ></b-form-input>
             </b-col>
         </b-row>
