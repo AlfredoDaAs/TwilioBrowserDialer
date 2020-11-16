198e4eaf98f342563dbbd64ac69de3d8f53366c0
Alfredo Dávila Assad <alfredo.davila@codigomx.io>
2020-11-15 22:10:28 -0600

    removed unused stuff

--- static-site/app/src/axios/index.js
+++ static-site/app/src/axios/index.js
@@ -2,18 +2,6 @@ import axios from 'axios'
 
 let axiosInstance = null
 
-// this is required because axios 0.19.* it's not merging params from create instance and the request params
-// if the bug is fixed this could be removed and uncomment line 25
-export const basicParams = () => {
-    const params = {}
-
-    if(process.env.NODE_ENV === 'development') {
-        params['token'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiYml0IiwiaWF0IjoxNTg2MzAwMjI4LCJleHAiOjE3NDQwODgyMjh9.MvdFZ254l5NQHdGx4mVUCnnV8r_wMUh9W01MZfZgHAE'
-    }
-
-    return params
-}
-
 const getInstance = () => {
     if(!axiosInstance) {
         let baseURL = ''
