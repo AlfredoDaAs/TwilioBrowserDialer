cd8a94b948608f2cff8ef5f40104cf6564e70727
Alfredo Dávila Assad <alfredo.davila@codigomx.io>
2020-11-15 21:52:08 -0600

    user crud tweaks

--- firebase-functions/functions/src/api/users/crud.ts
+++ firebase-functions/functions/src/api/users/crud.ts
@@ -4,11 +4,19 @@ import User from '../../firestore/users'
 
 const router = express.Router()
 
-router.get('/', async (req, res) => {
+router.get('/:id?', async (req, res) => {
     try {
-        const users = await User.getAllUsers()
+        const id = req.params.id
+        if(id) {
+            const user = await User.readOne(id)
 
-        res.json(users)
+            res.json(user)
+        }
+        else {
+            const users = await User.getAllUsers()
+
+            res.json(users)
+        }
     } catch (error) {
         throw new Error(error);
     }
@@ -18,10 +26,18 @@ router.post('/', adminMiddleware, async (req, res) => {
     try {
         const body = req.body
 
+        if(!body.name
+            || !body.lastname
+            || !body.email) {
+            throw new Error("Missing required fields (name, lastname, email)");
+        }
+
         const result = await User.createOne({
             name: body.name,
+            lastName: body.lastname,
             email: body.email,
-            'twilio.number': body.twilioNumber
+            phoneNumber: body.phoneNumber ? body.phoneNumber : null,
+            deparment: body.deparment ? body.deparment : null
         })
 
         res.json(result)
@@ -30,11 +46,25 @@ router.post('/', adminMiddleware, async (req, res) => {
     }
 })
 
-/* router.put('/:id', adminMiddleware, (req, res) => {
+router.put('/:id', adminMiddleware, async (req, res) => {
+    try {
+        const body = req.body
+        const id = req.params.id
 
+        const result = await User.updateOne(id, {
+            phoneNumber: body.phoneNumber,
+            deparment: body.deparment,
+            name: body.name,
+            lastName: body.lastName
+        })
+
+        res.json(result)
+    } catch (error) {
+        throw new Error(error);
+    }
 })
 
-router.delete('/:id', adminMiddleware, (req, res) => {
+/* router.delete('/:id', adminMiddleware, (req, res) => {
 
 }) */
 
