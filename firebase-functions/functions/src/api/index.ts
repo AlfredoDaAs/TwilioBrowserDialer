27c31bb9ab6da73ca19d7284c67db02dfd26487e
Alfredo Dávila Assad <alfredo.davila@codigomx.io>
2020-11-19 23:30:29 -0600

    clean code

--- firebase-functions/functions/src/api/index.ts
+++ firebase-functions/functions/src/api/index.ts
@@ -4,59 +4,59 @@ import * as jwt from 'jsonwebtoken'
 import User from '../firestore/users'
 
 export const apiValidation = (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    const auth = req.headers.authorization
-    
-    if(auth) {
-        const token = auth.split('Bearer ')[1]
-        
-        if(!token) {
-            return res.status(403)
-                .json({
-                    success: false,
-                    message: 'Failed to Authenticate Token'
-                })
-        }
-        
-        jwt.verify(token, functions.config().jwt.key, async (err: any, decoded: any) => {
-            if(err) {
-                return res.status(403)
-                    .json({
-                        success: false,
-                        message: 'Failed to Authenticate Token' 
-                    })
-            }
+  const auth = req.headers.authorization
 
-            req.decoded = decoded
+  if (auth) {
+    const token = auth.split('Bearer ')[1]
 
-            if(!decoded.email) {
-                return res.status(403)
-                    .json({
-                        success: false,
-                        message: 'No email account found'
-                    })
-            }
-
-            const user = await User.findByEmail(decoded.email)
-
-            if(!user) {
-                return res.status(403)
-                    .json({
-                        success: false,
-                        message: 'User does not exist'
-                    })
-            }
-
-            req.isAdmin = user.isAdmin
-            next()
-            return;
+    if (!token) {
+      return res.status(403)
+        .json({
+          success: false,
+          message: 'Failed to Authenticate Token'
         })
-
-        return;
     }
-    else {
-        return res.status(403).json({
+
+    jwt.verify(token, functions.config().jwt.key, async (err: any, decoded: any) => {
+      if (err) {
+        return res.status(403)
+          .json({
             success: false,
             message: 'Failed to Authenticate Token'
-        });
-    }
+          })
+      }
+
+      req.decoded = decoded
+
+      if (!decoded.email) {
+        return res.status(403)
+          .json({
+            success: false,
+            message: 'No email account found'
+          })
+      }
+
+      const user = await User.findByEmail(decoded.email)
+
+      if (!user) {
+        return res.status(403)
+          .json({
+            success: false,
+            message: 'User does not exist'
+          })
+      }
+
+      req.isAdmin = user.isAdmin
+      next()
+      return;
+    })
+
+    return;
+  }
+  else {
+    return res.status(403).json({
+      success: false,
+      message: 'Failed to Authenticate Token'
+    });
+  }
 }
