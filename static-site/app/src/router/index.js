4d1450e5d1d8940da59a5745375b0be6a4c84f1d
Alfredo Dávila Assad <alfredo.davila@codigomx.io>
2020-11-22 22:21:27 -0600

    added department path to router

--- static-site/app/src/router/index.js
+++ static-site/app/src/router/index.js
@@ -6,88 +6,96 @@ import store from '../store'
 Vue.use(VueRouter)
 
 const routes = [
-    {
-        path: '/',
-        name: 'home',
-        component: Home,
-        children: [
-            {
-                path: '/calls',
-                name: 'calls',
-                component: () => import('../views/pages/Calls')
-            },
-            {
-                path: '/users',
-                name: 'users',
-                meta: {
-                    is_admin: true // protects admin views from normal users
-                },
-                component: () => import('../views/pages/Users')
-            },
-            {
-              path: '/numbers',
-              name: 'numbers',
-              component: () => import('../views/pages/Numbers')
-            }
-        ]
-    },
-    {
-        path: '/sessions',
-        component: () => import('../views/sessions'),
-        redirect: '/sessions/login',
-        children: [
-            {
-                path: '/login',
-                name: 'login',
-                component: () => import('../views/sessions/login')
-            }
-        ]
-    },
-    {
-        path: "*",
-        component: () => import("../views/pages/notFound")
-    }
+  {
+    path: '/',
+    name: 'home',
+    component: Home,
+    children: [
+      {
+        path: '/calls',
+        name: 'calls',
+        component: () => import('../views/pages/Calls')
+      },
+      {
+        path: '/users',
+        name: 'users',
+        meta: {
+          is_admin: true // protects admin views from normal users
+        },
+        component: () => import('../views/pages/Users')
+      },
+      {
+        path: '/numbers',
+        name: 'numbers',
+        component: () => import('../views/pages/Numbers')
+      },
+      {
+        path: '/departments',
+        name: 'departments',
+        component: () => import('../views/pages/Deparments'),
+        meta: {
+          is_admin: true // protects admin views from normal users
+        },
+      }
+    ]
+  },
+  {
+    path: '/sessions',
+    component: () => import('../views/sessions'),
+    redirect: '/sessions/login',
+    children: [
+      {
+        path: '/login',
+        name: 'login',
+        component: () => import('../views/sessions/login')
+      }
+    ]
+  },
+  {
+    path: "*",
+    component: () => import("../views/pages/notFound")
+  }
 ]
 
 const router = new VueRouter({
-    routes
+  routes
 })
 
 const hasPermissions = (to) => {
-    if(to.meta.is_admin) {
-        if(store.getters.isAdmin) {
-            return true
-        }
-        
-        return false
+  if (to.meta.is_admin) {
+    if (store.getters.isAdmin) {
+      return true
     }
 
-    // not for admins so user has permission
-    return true
+    return false
+  }
+
+  // not for admins so user has permission
+  return true
 }
 
 router.beforeEach((to, from, next) => {
-    if(to.name === 'login') {
-        if(store.getters.isAuthenticated) {
-            next({ name: 'home' })
-        }
-        else {
-            next()
-        }
+  if (to.name === 'login') {
+    if (store.getters.isAuthenticated) {
+      next({ name: 'home' })
     }
     else {
-        if(store.getters.isAuthenticated) {
-            if(hasPermissions(to)) {
-                next()
-            }
-            else {
-                next({ name: 'home' })
-            }
-        }
-        else {
-            next({ name: 'login' })
-        }
+      next()
     }
+  }
+  else {
+    if (store.getters.isAuthenticated) {
+      if (hasPermissions(to)) {
+        next()
+      }
+      else {
+        next({ name: 'home' })
+      }
+    }
+    else {
+      next({ name: 'login' })
+    }
+  }
 })
 
 export default router
