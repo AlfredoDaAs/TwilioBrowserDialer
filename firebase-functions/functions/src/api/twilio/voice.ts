90cbc5f4dea0b88de53e6f6a28a87b92090a7d45
Alfredo Dávila Assad <alfredo.davila@codigomx.io>
2020-11-22 19:38:30 -0600

    twiml voice file moved inside twilio folder

---
+++ firebase-functions/functions/src/api/twilio/voice.ts
@@ -0,0 +1,54 @@
+import * as express from 'express'
+import * as functions from 'firebase-functions';
+import * as twilio from 'twilio'
+
+const router = express.Router()
+
+const VoiceResponse = twilio.twiml.VoiceResponse;
+
+const validateTwilioRequest = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    const twilioSignature = req.headers['x-twilio-signature']
+    const params = req.body
+    const url = functions.config().twilio.voiceurl // post endpoint
+
+    if(typeof twilioSignature === 'string') {
+        const requestIsValid = twilio.validateRequest(
+            functions.config().twilio.authtoken,
+            twilioSignature,
+            url,
+            params
+        )
+
+        if(requestIsValid) {
+            next()
+            return;
+        }
+    }
+
+    res.sendStatus(403)
+    return;
+}
+
+// Create TwiML for outbound calls
+router.post('/outbound', validateTwilioRequest, (request, response) => {
+    const voiceResponse = new VoiceResponse();
+    
+    voiceResponse.dial({
+        // this will need to update a more dynamic way
+        callerId: functions.config().twilio.phonenumber
+    }, request.body.number);
+
+    response.type('text/xml');
+    response.send(voiceResponse.toString());
+});
+
+router.post('/inbound', validateTwilioRequest, (req: express.Request, res: express.Response) => {
+  const voiceResponse = new VoiceResponse();
+
+  voiceResponse.say('Hello, this twilio browser dialer call center, wait for an agent to get your call...');
+
+  res.type('text/xml');
+  res.send(voiceResponse.toString());
+});
+
+export default router;
