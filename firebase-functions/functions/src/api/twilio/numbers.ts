a26f67ce492990ba181a404732385b2ce182828d
Alfredo Dávila Assad <alfredo.davila@codigomx.io>
2020-11-22 19:37:58 -0600

    search & provision routes

---
+++ firebase-functions/functions/src/api/twilio/numbers.ts
@@ -0,0 +1,37 @@
+import * as express from 'express';
+import * as functions from 'firebase-functions';
+import * as twilio from 'twilio';
+import users from '../../firestore/users';
+
+const router = express.Router();
+
+const accountSid = functions.config().twilio.accountsid;
+const authToken = functions.config().twilio.authtoken;
+
+const client = twilio(accountSid, authToken);
+
+router.get('/search', async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  try {
+    const availablePhoneNumbers = await client.availablePhoneNumbers('US').local.list({ limit: 20 });
+
+    res.json(availablePhoneNumbers)
+  } catch (error) {
+    next(error)
+  }
+});
+
+router.post('/provision', async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  try {
+    const voiceUrl = functions.config().twilio.voiceurl
+    const incomingPhoneNumber = await client.incomingPhoneNumbers.create({ phoneNumber: req.body.phoneNumber, voiceUrl })
+    const result = await users.updateOne(req.decoded.id, {
+      phoneNumber: incomingPhoneNumber.phoneNumber
+    })
+
+    res.json(result)
+  } catch (error) {
+    next(error)
+  }
+});
+
+export default router;
