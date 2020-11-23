a0ff3889267768360bb9b960128d570587b43134
Alfredo Dávila Assad <alfredo.davila@codigomx.io>
2020-11-22 19:39:09 -0600

    handle any type of messages

--- static-site/app/src/handleErrors.js
+++ static-site/app/src/handleErrors.js
@@ -1,27 +1,31 @@
 import store from './store'
 
-export const handleError = (error, comp, variant) =>  {
-    if(error.response && error.response.status === 403) {
-        // do logout
-        store.dispatch('logout')
+export const handleMessage = (message, comp, variant) => {
+  comp.$bvToast.toast(message, {
+    title: 'Message',
+    variant: variant,
+    autoHideDelay: 5000,
+    solid: true,
+    appendToast: true,
+    toaster: 'b-toaster-top-right'
+  })
+}
+
+export const handleError = (error, comp, variant) => {
+  if (error.response && error.response.status === 403) {
+    // do logout
+    store.dispatch('logout')
+  }
+  else {
+    // toast message
+    let message = ''
+    if (error.response && error.response.status === 500) {
+      message = error.response.data.error
     }
     else {
-        // toast message
-        let message = ''
-        if(error.response && error.response.status === 500) {
-            message = error.response.data.error
-        }
-        else {
-            message = error.message
-        }
-
-        comp.$bvToast.toast(message, {
-            title: 'Message',
-            variant: variant,
-            autoHideDelay: 5000,
-            solid: true,
-            appendToast: true,
-            toaster: 'b-toaster-top-right'
-          })
+      message = error.message
     }
+
+    handleMessage(message, comp, variant);
+  }
 }
