f16ede11a2d387193e8315daa14e9a7672f8f85e
Alfredo Dávila Assad <alfredo.davila@codigomx.io>
2020-12-06 23:04:08 -0600

    call collection update

--- firebase-functions/functions/src/firestore/calls/index.ts
+++ firebase-functions/functions/src/firestore/calls/index.ts
@@ -4,8 +4,23 @@ const collectionName = 'calls'
 
 class Collection extends AbstractCollection {
   public defaultValues: any = {
-    from: null,
-    to: null
+    fromData: {
+      from: null,
+      city: null,
+      country: null,
+      state: null,
+      zip: null
+    },
+    toData: {
+      to: null,
+      city: null,
+      country: null,
+      state: null,
+      zip: null
+    },
+    direction: 'inbound' || 'outbound',
+    transfers: [],
+    callSid: null
   }
 
   public async getCallsOfNumber(number: string): Promise<any[]> {
