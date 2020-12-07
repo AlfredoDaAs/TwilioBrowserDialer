f9fabc1840dd4ce864cc836b3ae7a52e0d4a4f4b
Alfredo Dávila Assad <alfredo.davila@codigomx.io>
2020-12-06 23:05:05 -0600

    incoming call modal

---
+++ static-site/app/src/components/IncomingCallModal.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+  name: 'IncomingCallModal',
+  props: {
+    from: {
+      type: String,
+      default: ''
+    },
+    show: {
+      type: Boolean,
+      default: false,
+    }
+  },
+  methods: {
+    handleOk(e) {
+      if(e) e.preventDefault()
+
+      this.takeCall();
+    },
+    handleCancel(e) {
+      if(e) e.preventDefault();
+
+      this.$emit('onCancel');
+    },
+    takeCall() {
+      this.$emit('onAccept');
+    }
+  }
+}
+</script>
+
+<template>
+  <b-modal
+    :visible="show"
+    title="Incoming Call"
+    ok-title="Take Call"
+    ok-variant="success"
+    @ok="handleOk"
+    @cancel="handleCancel"
+    no-close-on-esc
+    no-close-on-backdrop
+  >
+    <p>Call From {{ from }}</p>
+  </b-modal>
+</template>
