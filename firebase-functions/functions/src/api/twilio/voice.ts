ddf553433fc96d2f899efc983ea323dedb3e6d9e
Alfredo Dávila Assad <alfredo.davila@codigomx.io>
2020-12-06 23:02:44 -0600

    dynamic inbound/outbound

--- firebase-functions/functions/src/api/twilio/voice.ts
+++ firebase-functions/functions/src/api/twilio/voice.ts
@@ -1,51 +1,76 @@
 import * as express from 'express'
 import * as functions from 'firebase-functions';
 import * as twilio from 'twilio'
+import calls from '../../firestore/calls'
 
 const router = express.Router()
 
 const VoiceResponse = twilio.twiml.VoiceResponse;
 
 const validateTwilioRequest = (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    const twilioSignature = req.headers['x-twilio-signature']
-    const params = req.body
-    const url = functions.config().twilio.voiceurl // post endpoint
+  const twilioSignature = req.headers['x-twilio-signature']
+  const params = req.body
+  const url: String = functions.config().twilio.voiceurl // post endpoint
 
-    if(typeof twilioSignature === 'string') {
-        const requestIsValid = twilio.validateRequest(
-            functions.config().twilio.authtoken,
-            twilioSignature,
-            url,
-            params
-        )
+  if (typeof twilioSignature === 'string') {
+    const requestIsValid = twilio.validateRequest(
+      functions.config().twilio.authtoken,
+      twilioSignature,
+      `${url}${req.path}?clientName=${req.query.clientName}`,
+      params
+    )
 
-        if(requestIsValid) {
-            next()
-            return;
-        }
+    if (requestIsValid) {
+      next()
+      return;
     }
+  }
 
-    res.sendStatus(403)
-    return;
+  res.sendStatus(403)
+  return;
 }
 
 // Create TwiML for outbound calls
-router.post('/outbound', validateTwilioRequest, (request, response) => {
-    const voiceResponse = new VoiceResponse();
-    
-    voiceResponse.dial({
-        // this will need to update a more dynamic way
-        callerId: functions.config().twilio.phonenumber
-    }, request.body.number);
-
-    response.type('text/xml');
-    response.send(voiceResponse.toString());
-});
-
-router.post('/inbound', validateTwilioRequest, (req: express.Request, res: express.Response) => {
+router.post('/outbound', validateTwilioRequest, (req: express.Request, res: express.Response) => {
   const voiceResponse = new VoiceResponse();
 
-  voiceResponse.say('Hello, this twilio browser dialer call center, wait for an agent to get your call...');
+  voiceResponse.dial({
+    callerId: req.body.callerId
+  }, req.body.number);
+
+  res.type('text/xml');
+  res.send(voiceResponse.toString());
+});
+
+router.post('/inbound', validateTwilioRequest, async (req: express.Request, res: express.Response) => {
+  const result = await calls.createOne({
+    fromData: {
+      from: req.body.From,
+      city: req.body.FromCity,
+      country: req.body.FromCountry,
+      state: req.body.FromState,
+      zip: req.body.FromZip
+    },
+    toData: {
+      to: req.body.To,
+      city: req.body.ToCity,
+      country: req.body.ToCountry,
+      state: req.body.ToState,
+      zip: req.body.ToZip
+    },
+    callSid:  req.body.CallSid,
+    direction: 'inbound'
+  })
+
+  const clientName = req.query.clientName as string
+  const voiceResponse = new VoiceResponse();
+  const dial = voiceResponse.dial();
+  const client = dial.client(clientName)
+
+  client.parameter({
+    name: 'callId',
+    value: result
+  })
 
   res.type('text/xml');
   res.send(voiceResponse.toString());
