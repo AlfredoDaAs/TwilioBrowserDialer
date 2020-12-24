a1aee503896ca9b9d7502a4b47af911fd13421cc
Alfredo Dávila Assad <alfredo.davila@codigomx.io>
2020-12-23 22:34:24 -0600

    init worker with token

--- static-site/app/src/twilio/index.js
+++ static-site/app/src/twilio/index.js
@@ -1,16 +1,25 @@
 import { Device } from 'twilio-client';
 
 export const initDevice = (token) => {
-    Device.setup(token, {
-      debug: true,
-      enableRingingState: true
-    })
+  Device.setup(token, {
+    debug: true,
+    enableRingingState: true
+  })
 
   Device.on('ready', () => {
-        console.log('Connected');
-    })
+    console.log('Connected');
+  })
 }
 
 export const destroyDevice = () => {
   Device.destroy();
+}
+
+
+export function initWorker(token) {
+  try {
+    return new Twilio.TaskRouter.Worker(token); 
+  } catch (error) {
+    console.error(error);
+  }
 }
