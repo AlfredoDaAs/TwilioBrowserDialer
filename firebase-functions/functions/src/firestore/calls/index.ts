c0850a3caa9e24cd67a048a746e5ff5d91a4453e
Alfredo Dávila Assad <alfredo.davila@codigomx.io>
2021-01-16 02:39:39 -0600

    updates in calls collection model

--- firebase-functions/functions/src/firestore/calls/index.ts
+++ firebase-functions/functions/src/firestore/calls/index.ts
@@ -10,19 +10,36 @@ class Collection extends AbstractCollection {
     transfers: [],
     conferenceSid: null,
     callSid: null,
-    callTime: null
+    callDuration: null
   }
 
   public async getCallsOfNumber(number: string): Promise<any[]> {
-    const results = (await this.collection.where('from', '==', number).where('to', '==', number).get()).docs
+    const from = (await this.collection.where('from', '==', number).get()).docs.map((doc:any) => doc.data())
+    const to = (await this.collection.where('to', '==', number).get()).docs.map((doc:any) => doc.data())
 
-    return results.map((doc:any) => doc.data())
+    const results = from.concat(to)
+
+    results.sort((a:any,b:any)=> {
+        return (new Date(b.createdAt).getTime()) - (new Date(a.createdAt).getTime())
+    })
+
+    return results
   }
 
   public async getCalls(): Promise<any[]> {
     const results = (await this.collection.limit(20).orderBy('createdAt', 'desc').get()).docs
 
     return results.map((doc:any) => doc.data())
+  }
+
+  public async getCallBySid(sid: string): Promise<any> {
+    const result = (await this.collection.where('callSid', '==', sid).get()).docs
+
+    if(result.length === 0) {
+        return null;
+    }
+
+    return result[0].data()
   }
 }
 
